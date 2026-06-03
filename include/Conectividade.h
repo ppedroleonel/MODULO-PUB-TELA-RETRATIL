7bdaeff78cc4d37130fcc5fb37d7332df951dc57
PEDRO LEONEL DE LORENA <53955089886@info.local>
2026-06-03 16:37:46 -0300

    Versão 1.0.0: Criado o .h com a biblioteca ESP32Connectivy ja integrada

---
+++ include/Conectividade.h
@@ -0,0 +1,11 @@
+#ifndef Conectividade_h
+#define Conectividade_h
+
+#include <Arduino.h>
+#include <ESP32Connectivity.h>
+
+extern ConfigWiFi wifiConfig;
+extern ConfigAWS awsConfig;
+extern ConfigTopicos topicosConfig;
+
+#endif
