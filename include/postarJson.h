00f1e69421c9acf637e677feb646d133a9cfe3ca
PEDRO LEONEL DE LORENA <53955089886@info.local>
2026-06-03 16:38:28 -0300

    Versão 1.0.0: Criado um .h pro postarJson pois não existe mais o mqttManager

---
+++ include/postarJson.h
@@ -0,0 +1,12 @@
+#ifndef postarJson_h
+#define postarJson_h
+#include <Arduino.h>
+
+extern ESP32Connectivity telaRetratil;
+extern int8_t tela;
+
+void postarBotaoDown();
+void postarBotaoUp();
+void postarBotaoPause();
+
+#endif
