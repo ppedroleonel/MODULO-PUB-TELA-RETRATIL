6cba3a74bc27a0854c46f7e4ae247ba3bf980c55
PEDRO LEONEL DE LORENA <53955089886@info.local>
2026-06-03 16:42:57 -0300

    Versão 1.0.0: Modificado o secrets.h para so existir o que a bilbioteca precisa

--- include/secrets.h
+++ include/secrets.h
@@ -1,67 +1,20 @@
-//!secrets.
-
 #ifndef SECRETS_H
 #define SECRETS_H
 
-// * CONFIGURAÇÕES DO WIFI
+#include <Arduino.h>
+#include <ESP32Connectivity.h>
 
 extern const char *WIFI_SSID;
 extern const char *WIFI_SENHA;
 
-//======================================
-// *MQTT
-//======================================
+extern const char *awsCertCA;
+extern const char *awsCertCRT;
+extern const char *awsCertPrivate;
 
-extern const bool USAR_AWS_IOT;
+extern const char* pub[];
+extern const char* rec[];
 
-extern const char* MQTT_BROKER;
-extern const int MQTT_PORTA;
-
-// o ID do mqtt sempre vai ser diferente
-// nunca criar um id igual de alguem pois ele nao permite e fica desconectando um
-extern const char* MQTT_CLIENT_ID;
-
-extern const char* MQTT_USUARIO;
-extern const char* MQTT_SENHA;
-
-// essa extern const fala q nao estamos usando certificado
-// modo de conexao sem segurança
-extern const bool MQTT_USAR_TLS;
-
-extern const  char MQTT_CERTIFICADO_CA[];
-extern const char* TOPICOS_PUBLICAR[];
-
-// * AWS
-
-extern const char AWS_CERT_CA[];
-extern const char AWS_CERT_CRT[];
-extern const char AWS_CERT_PRIVATE[];
-extern const char* AWS_IOT_ENDPOINT;
-extern const int AWS_IOT_PORT;
-extern const char* AWS_IOT_CLIENT_ID;
-
-
-
-extern const int TOTAL_TOPICOS_PUBLICAR;
-
-// 
-extern const char* TOPICOS_RECEBER[];
-
-extern const int TOTAL_TOPICOS_RECEBER;
-
-extern const char* TOPICO_COMANDO;
-
-//======================================
-// *DEBUG
-//======================================
-
-// 0 = sem mensagens
-// 1 = apenas erros
-// 2 = todas mensagens
 extern const int DEBUG_NIVEL_INICIAL;
 
-// Pino usado para forçar todas as mensagens na inicialização
 extern const int PINO_HABILITAR_DEBUG_COMPLETO;
-
-
 #endif
